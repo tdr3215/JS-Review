1ed619ec24570d0f5a9d526dd0493edb9801c7d9
Tiffany Rose <tiffany.drose@gmail.com>
2023-04-18 03:39:38 +0100

    organized starter and solution

---
+++ dom-trav/practice/starter.js
@@ -0,0 +1,43 @@
+// ADD A NEW CARD TO CONTAINER
+
+// Create variable for container div
+// *must have something on the page to append new el to*
+// var containerUl = document.querySelector(".container");
+
+//  ** Steps in creating a new HTML element **
+
+// - Create new element
+// var newCard = document.createElement("div");
+
+// -If there are attributes associated with this element, add them here
+// newCard.setAttribute("class", "card");
+
+// - Add content to the new element
+// newCard.textContent = "Card 4"; // - good for unknown values
+
+// optional alternative --> newCard.innerHTML("<span>Card 4</span>")
+
+// - Append new element to something that already exists on the page
+// containerUl.append(newCard);
+
+// -----------------------------------------------------------------------------------
+
+// CODING CHALLENGE - Create a form that adds a new card based on user input
+
+// What do we need to do?
+// - Create a button and append to page
+// - Create an button and append to page
+// - Create function(s) to execute on click
+// - Create a 'click' event listener
+
+// Variables
+
+// 1st Step: Create and append button element
+
+// 2nd Step: Create functions for click event
+function renderCards() {}
+
+function handleInput(e) {}
+
+// 3rd Step: Add event listener
+formDiv.addEventListener("submit", function () {});
